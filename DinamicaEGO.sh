f9a2dac3c8622841be63066092faa8fac9768669
fabricio-fernandes <fabriciofernandes3f@gmail.com>
2024-09-15 20:39:55 -0300

    fix import python modules

---
+++ DinamicaEGO.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Algumas variáveis de ambiente usadas aqui são especificadas do AppImage.
+# APPDIR: Path of mountpoint of the SquashFS image contained in the AppImage.
+# OWD:    Path to working directory at the time the AppImage is called.
+
+DINAMICA_HOME="${HOME}/Dinamica EGO 7"
+REGISTRY_FILE="${HOME}/.dinamica_ego_7.conf"
+SYSTEM_REGISTRY_FILE="${HOME}/.dinamica_ego_7_system.conf"
+
+BIN_PATH=`dirname "$0"`
+
+DINAMICA_GUI_COMMAND="${BIN_PATH}/jre/bin/java -Djava.library.path=${BIN_PATH}/../lib/ -DrPluginFilename=Dinamica_1.0.7.tar.gz -Xss1m -Xms64m -Xmx8192m -server -Dawt.useSystemAAFontSettings=lcd -ea -Djava.util.Arrays.useLegacyMergeSort=true -Dsun.java2d.noddraw=false -Dsun.java2d.d3d=false -Dprism.order=sw -splash:$APPDIR/Splash.png -jar ${BIN_PATH}/DinamicaNUI.jar"
+
+DINAMICA_CONSOLE_COMMAND=bin/DinamicaConsole
+
+export DINAMICA_EGO_7_INSTALLATION_DIRECTORY=${BIN_PATH}
+
+# Cria os arquivos de registro, se eles não existirem
+if [ ! -e "${REGISTRY_FILE}" ]; then
+  touch ${REGISTRY_FILE}
+fi
+if [ ! -e "${SYSTEM_REGISTRY_FILE}" ]; then
+  touch ${SYSTEM_REGISTRY_FILE}
+fi
+
+if [ $# -eq 0 ]; then
+  export DINAMICA_EGO_7_LOG_PATH=${DINAMICA_HOME}
+  exec ${DINAMICA_GUI_COMMAND}
+
+else
+  # Caso o nome de arquivo do script não seja passado e seja passada opção, define o folder padrão do usuário como
+  # folder de log. Caso contrário, o folder de log será o folder contendo o arquivo do script sendo executado.
+  # Além disso, o nome do arquivo do script é completado, caso ele seja relativo.
+
+  if [[ "${!#}" = -* ]]; then
+    # Argumento é opção.
+    export DINAMICA_EGO_7_LOG_PATH=${DINAMICA_HOME}
+    exec ${DINAMICA_CONSOLE_COMMAND} "$@"
+
+  elif [[ "${!#}" = /* ]]; then
+    # Argumento é nome de script com caminho absoluto: não é necessário completar o caminho.
+    export DINAMICA_EGO_7_LOG_PATH=`dirname "${!#}"`
+    exec ${DINAMICA_CONSOLE_COMMAND} "$@"
+
+  else
+    # Argumento é nome de script com caminho relativo: é necessário completar o caminho.
+    export DINAMICA_EGO_7_LOG_PATH=`dirname "$OWD/${!#}"`
+    exec ${DINAMICA_CONSOLE_COMMAND} "${@:1:$#-1}" "$OWD/${!#}"
+  fi
+fi
+
--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,26 @@
 FROM ubuntu:latest
 
-RUN apt update && apt upgrade
-RUN apt install curl libfuse2t64
+#Install dependecies
+RUN apt update && apt upgrade -y
 
-RUN curl https://dinamicaego.com/nui_download/1792/ -o DinamicaEgo-780.AppImage
+#For some reason whem python calculute expression import some libraries like rasterio it crash beacause libexpat1 can't be found
+#Installing it with apt was the solution found
 
+RUN apt install curl libexpat1 -y
 
+WORKDIR /root/DinamicaEgo/
+COPY ./tests/ tests/
+
+#Install DinamcaEGO
+RUN curl https://dinamicaego.com/nui_download/1792/ -o ./DinamicaEgo-780.AppImage \
+
+        #Grant excution permision
+        && chmod +x ./DinamicaEgo.AppImage \
+        
+        #Extract AppImage
+        && ./DinamicaEgo.AppImage --appimage-extract \
+        #Remove the AppImage
+        && rm -rf ./DinamicaEgo.AppImage
+
+#DinamicaConsole tem problemas com nome de arquivos diretamente no funtor. Devem ser fornecidos por um filename
+ENTRYPOINT [ "/bin/bash" ]
