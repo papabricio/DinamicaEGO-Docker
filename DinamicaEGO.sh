6d7a3c89d0b358830341476dee13130b2aace561
fabricio-fernandes <fabriciofernandes3f@gmail.com>
2024-09-16 00:06:00 -0300

    change logs folder

--- DinamicaEGO.sh
+++ DinamicaEGO.sh
@@ -10,7 +10,7 @@ SYSTEM_REGISTRY_FILE="${HOME}/.dinamica_ego_7_system.conf"
 
 BIN_PATH=`dirname "$0"`
 
-DINAMICA_GUI_COMMAND="${BIN_PATH}/jre/bin/java -Djava.library.path=${BIN_PATH}/../lib/ -DrPluginFilename=Dinamica_1.0.7.tar.gz -Xss1m -Xms64m -Xmx8192m -server -Dawt.useSystemAAFontSettings=lcd -ea -Djava.util.Arrays.useLegacyMergeSort=true -Dsun.java2d.noddraw=false -Dsun.java2d.d3d=false -Dprism.order=sw -splash:$APPDIR/Splash.png -jar ${BIN_PATH}/DinamicaNUI.jar"
+#DINAMICA_GUI_COMMAND="${BIN_PATH}/jre/bin/java -Djava.library.path=${BIN_PATH}/../lib/ -DrPluginFilename=Dinamica_1.0.7.tar.gz -Xss1m -Xms64m -Xmx8192m -server -Dawt.useSystemAAFontSettings=lcd -ea -Djava.util.Arrays.useLegacyMergeSort=true -Dsun.java2d.noddraw=false -Dsun.java2d.d3d=false -Dprism.order=sw -splash:$APPDIR/Splash.png -jar ${BIN_PATH}/DinamicaNUI.jar"
 
 DINAMICA_CONSOLE_COMMAND=bin/DinamicaConsole
 
@@ -25,8 +25,11 @@ if [ ! -e "${SYSTEM_REGISTRY_FILE}" ]; then
 fi
 
 if [ $# -eq 0 ]; then
+  echo "No model filename was given"
+
+  #Define o folder do usuário como folder de log
   export DINAMICA_EGO_7_LOG_PATH=${DINAMICA_HOME}
-  exec ${DINAMICA_GUI_COMMAND}
+  exec ${DINAMICA_CONSOLE_COMMAND} -help
 
 else
   # Caso o nome de arquivo do script não seja passado e seja passada opção, define o folder padrão do usuário como
@@ -40,12 +43,12 @@ else
 
   elif [[ "${!#}" = /* ]]; then
     # Argumento é nome de script com caminho absoluto: não é necessário completar o caminho.
-    export DINAMICA_EGO_7_LOG_PATH=`dirname "${!#}"`
+    export DINAMICA_EGO_7_LOG_PATH=${DINAMICA_HOME}
     exec ${DINAMICA_CONSOLE_COMMAND} "$@"
 
   else
     # Argumento é nome de script com caminho relativo: é necessário completar o caminho.
-    export DINAMICA_EGO_7_LOG_PATH=`dirname "$OWD/${!#}"`
+    export DINAMICA_EGO_7_LOG_PATH=${DINAMICA_HOME}
     exec ${DINAMICA_CONSOLE_COMMAND} "${@:1:$#-1}" "$OWD/${!#}"
   fi
 fi
